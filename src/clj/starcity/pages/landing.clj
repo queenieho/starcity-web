40ba1928b4e2bdb99fb508edd41fdebfe92e5bb3
Josh Lehman <jalehman37@gmail.com>
2016-04-28 11:40:20 -0700

    add placeholder index page, close #17

--- src/clj/starcity/pages/landing.clj
+++ src/clj/starcity/pages/landing.clj
@@ -9,10 +9,31 @@
 ;; =============================================================================
 ;; Components
 
+(defn- landing-content []
+  [:div.site-wrapper
+   [:div.site-wrapper-inner
+    [:div.cover-container
+     [:div.masthead.clearfix
+      [:div.inner
+       [:h3.masthead-brand "Starcity"]
+       [:nav
+        [:ul.nav.masthead-nav
+         [:li.active [:a {:href "/"} "Home"]]
+         [:li [:a {:href "#About"} "About"]]
+         [:li [:a {:href "#contact"} "Contact"]]]]]]
+
+     [:div.inner.cover
+      [:h1.cover-heading "Starcity"]
+      [:p.lead "Coming Soon."]
+      [:p.lead
+       [:a.btn.btn-lg.btn-default {:href "#"} "Learn more"]]]
+
+     [:div.mastfoot
+      [:div.inner
+       [:p "Yep."]]]]]])
+
 (defn- landing-view []
-  (base
-   [:div#app
-    [:h1 "Hello, Starcity!"]]))
+  (base (landing-content) :css ["cover.css"]))
 
 ;; =============================================================================
 ;; API
