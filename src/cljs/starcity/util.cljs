785e1c59a3bb953f4fe82cb87ea27628de3e70ab
Josh Lehman <jalehman37@gmail.com>
2016-05-18 10:36:40 -0700

    add logging utilities

--- src/cljs/starcity/util.cljs
+++ src/cljs/starcity/util.cljs
@@ -7,3 +7,12 @@
 
 (defn event-value [event]
   (.. event -target -value))
+
+(defn log [& args]
+  (.apply js/console.log js/console (to-array args)))
+
+(defn warn [& args]
+  (.apply js/console.warn js/console (to-array args)))
+
+(defn error [& args]
+  (.apply js/console.error js/console (to-array args)))
