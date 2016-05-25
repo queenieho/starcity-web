93099c9b90a0a096876a8c009d6bcd83c983ad8e
Josh Lehman <jalehman37@gmail.com>
2016-05-25 15:33:11 -0700

    try using config in project root?

--- src/clj/starcity/config.clj
+++ src/clj/starcity/config.clj
@@ -4,5 +4,5 @@
 (defn config [profile]
   (assert (#{:production :development} profile)
           (format "Profile must be one of #{:production :development}, not %s!" profile))
-  (-> (read-config "resources/config/config.edn" {:profile profile})
+  (-> (read-config "config.edn" {:profile profile})
       (assoc :profile profile)))
