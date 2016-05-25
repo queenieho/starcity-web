ce10dbe80dac0632527bf764536594248087b7b4
Mo Sakrani <msakrani@gmail.com>
2016-05-25 11:54:27 -0700

    v2 of landing page - introduces some copy - this is temporary

--- src/clj/starcity/pages/landing.clj
+++ src/clj/starcity/pages/landing.clj
@@ -15,8 +15,22 @@
     [:div.navbar-inner
      [:div.container
       [:a.brand {:href "/"}
-       [:img {:alt "Starcity" :src "/assets/img/starcity-logo.png"}]
-       [:a#login_btn.btn.btn-navbar {:href "/login"} "Log in"]]]]]])
+       [:img {:alt "Starcity" :src "/assets/img/starcity-logo-icon.png"}]
+       [:a#login_btn.btn.btn-navbar {:href "/login"} "Sign in"]
+       ]]]]
+   [:div#page.container
+    [:div.homepage
+     [:div.introsection
+      [:div.introsignedout
+       [:div.introcontent
+        [:div.introslogan
+         [:h1 "Reimagining Urban Living"]
+         [:h2 "What does home mean to you?"]
+         [:p "Help shape community-centric housing in San Francisco."]]
+        [:div.form-container 
+         [:form {:action "#" :method "GET"}
+          [:input.email-field {:type "text" :name "email" :id "user_email" :placeholder "Enter your email address"}]
+          [:input.submit-btn {:type "submit" :value "Join us"}]]]]]]]]])
 
 (defn- landing-view []
   (base (landing-content) :css ["landing.css"] :body-class "starcity-page-landing"))
