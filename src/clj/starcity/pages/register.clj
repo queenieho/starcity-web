23a8e4a2c545a6528dd4acf97483beea50959548
Mo Sakrani <msakrani@gmail.com>
2016-06-13 12:48:23 -0700

    hi-res logo, real text content, register page and content

--- src/clj/starcity/pages/register.clj
+++ src/clj/starcity/pages/register.clj
@@ -16,12 +16,24 @@
 ;; =============================================================================
 ;; Components
 
+(def navbar
+  [:nav.navbar
+   [:div.container
+    [:div.navbar-header
+     [:a.navbar-brand {:href "#"}
+      [:img {:alt "Starcity" :src "/assets/img/starcity-brand-icon-white.png"}]]]]])
+
 (defn- content [{:keys [params] :as req}]
-  [:h3 (format "Prepare for spam, %s!" (:email params))])
+  [:div.navbar-wrapper
+   [:div.container-fluid
+    navbar
+
+    [:div.container
+     [:h3 (format "Thank you for getting involved, %s!" (:email params)) " With your help, we'll design and build community-focused housing that will keep San Francisco's diversity right here, in our city. <br> <br> Let's work together to make San Francisco a financially attainable place to live by adding new housing supply to our city. Be on the lookout for an email with more information on how to participate."]]]])
 
 (defn- view [req]
   (base (content req)
-        :css ["landing.css"]            ; TODO:
+        :css ["register.css"]            ; TODO:
         ))
 
 ;; =============================================================================
