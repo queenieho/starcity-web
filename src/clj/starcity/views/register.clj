35071cadf5e61589c37d33bf655d14684697f2dd
Josh Lehman <jalehman37@gmail.com>
2016-07-06 11:11:21 -0700

    spruce up registration and signup comp pages

--- src/clj/starcity/views/register.clj
+++ src/clj/starcity/views/register.clj
@@ -1,31 +1,24 @@
 (ns starcity.views.register
   (:require [starcity.views.base :refer [base]]))
 
-;; TODO: The navbar has been repeated a few times...
-(def ^:private navbar
-  [:nav.navbar
-   [:div.container
-    [:div.navbar-header
-     [:a.navbar-brand {:href "#"}
-      [:img {:alt "Starcity" :src "/assets/img/starcity-brand-icon-white.png"}]]]]])
+;; =============================================================================
+;; Helpers
+;; =============================================================================
+
 
 ;; =============================================================================
 ;; API
 ;; =============================================================================
 
-(defn post-registration
+(defn registration
   [email]
   (base
-   [:div.navbar-wrapper
-    [:div.container-fluid
-     navbar
-     [:div.container
-      [:h3
-       ;; TODO: Clean up this HTML...<br> is bad.
-       (str (format "Thank you for getting involved, %s!" email)
-            " With your help, we'll design and build community-focused housing
-      that will keep San Francisco's diversity right here, in our city. <br>
-      <br> Let's work together to make San Francisco a financially attainable
+   [:div.container
+    [:div.page-header
+     [:h1 (format "Thank you for getting involved, <b>%s</b>!" email)]]
+    [:div
+     [:p.lead "With your help, we'll design and build community-focused housing
+      that will keep San Francisco's diversity right here, in our city. "]
+     [:p.lead "Let's work together to make San Francisco a financially attainable
       place to live by adding new housing supply to our city. Be on the lookout
-      for an email with more information on how to participate.")]]]]
-   :css ["register.css"]))
+      for an email with more information on how to participate."]]]))
