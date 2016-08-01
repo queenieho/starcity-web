063eda6194462f7972888bbc4c2754e75474b6b8
Josh Lehman <jalehman37@gmail.com>
2016-08-01 11:36:17 -0700

    close #188

--- src/clj/starcity/views/landing.clj
+++ src/clj/starcity/views/landing.clj
@@ -8,48 +8,18 @@
 ;; Helpers
 ;; =============================================================================
 
-(defn- promo-section
-  ([title text img-url]
-   (promo-section title text img-url :right))
-  ([title text img-url img-orientation]
-   (let [lcontent (if (= img-orientation :right)
-                    [:div.col.m7
-                     [:h3 title]
-                     [:p.flow-text text]]
-                    [:div.col.m5
-                     [:img.responsive-img {:src img-url}]])
-         rcontent (if (= img-orientation :right)
-                    [:div.col.m5
-                     [:img.responsive-img {:src img-url}]]
-                    [:div.col.m7
-                     [:h3 title]
-                     [:p.flow-text text]])]
-     [:section
-      [:div.row.valign-wrapper.hide-on-small-only
-       lcontent rcontent]
-
-      [:div.row.hide-on-med-and-up
-       [:div.col.s12
-        [:h3 title]
-        [:p.flow-text text]]
-
-       [:div.col.s12
-        [:img.responsive-img {:src img-url}]]]
-
-      [:div.divider]])))
-
-;; =============================================================================
-;; API
-;; =============================================================================
-
-;; TODO: rm -r resources/public/js/bower
-
 (defn- feature
   [title img-src blurb]
   [:div.col.m8.offset-m2.l4.center-align
    [:img.circle.responsive-img {:src img-src}]
    [:h5.center title]
    [:p.light blurb]])
+
+;; =============================================================================
+;; API
+;; =============================================================================
+
+;; TODO: rm -r resources/public/js/bower
 
 (def ^:private landing-content
   [:div
@@ -91,7 +61,9 @@
         [:p.flow-text.center "Enter your email to receive updates on Starcity's upcoming housing communities and events."]]
        [:div.row
         [:div.input-field
-         [:input#email.validate {:type "email" :required true}]
+         [:input#email.validate {:type     "email"
+                                 :name     "email"
+                                 :required true}]
          [:label {:for "email"} "Email"]]]
        [:div.row
         [:div.col.s12.center-align
