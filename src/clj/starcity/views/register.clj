d09c3fc52b497c8d08088f5affc2a2d10b8a63d9
Mo Sakrani <msakrani@gmail.com>
2016-08-02 10:17:31 -0700

    tweaks copy on /register

--- src/clj/starcity/views/register.clj
+++ src/clj/starcity/views/register.clj
@@ -20,7 +20,7 @@
      [:div.divider]
      [:div
       [:p.flow-text "With your help, we'll design and build community-focused housing
-      that will keep San Francisco's diversity right here, in our city. "]
-      [:p.flow-text "Let's work together to make San Francisco a financially attainable
-      place to live by adding new housing supply to our city. Be on the lookout
+      that will enable more San Franciscans to stay in our city. "]
+      [:p.flow-text "Let's work together to make San Francisco an attainable
+      place to live by creating welcoming communities the embrace the city's diversity and culture. Be on the lookout
       for an email with more information on how to participate."]]]]))
