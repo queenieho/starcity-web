536d549f58a4ebb4587cb3606691009b53fd94e0
Josh Lehman <jalehman37@gmail.com>
2016-08-09 18:37:46 -0700

    script fixes

--- script/build-assets.sh
+++ script/build-assets.sh
@@ -4,5 +4,6 @@ echo "Installing external JS & CSS dependencies via Bower..."
 bower install
 echo "Compiling Sass..."
 sass resources/public/assets/scss/main.scss:resources/public/assets/css/main.css
+cd elm
 echo "Compiling Elm Admin app..."
-elm-make elm/Admin.elm --output resources/public/js/elm/admin.js
+elm-make Admin.elm --yes --output ../resources/public/js/elm/admin.js
