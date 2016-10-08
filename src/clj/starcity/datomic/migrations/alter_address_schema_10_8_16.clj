2b0d731da2b9b552b49bbad049840af47b5897d7
Josh Lehman <jalehman37@gmail.com>
2016-10-08 14:24:52 -0700

    fix db environment seed issue

--- src/clj/starcity/datomic/migrations/alter_address_schema_10_8_16.clj
+++ src/clj/starcity/datomic/migrations/alter_address_schema_10_8_16.clj
@@ -1,34 +1,38 @@
 (ns starcity.datomic.migrations.alter-address-schema-10-8-16
   "This alteration adds support for international addresses."
   (:require [starcity.models.util :refer [find-all-by]]
+            [starcity.environment :refer [is-development?]]
             [datomic.api :as d]))
 
 (def alter-address-schema
-  {:schema/alter-address-schema-10-8-16
-   {:txes     [[{:db/id    :address/city
-                 :db/ident :address/locality
-                 :db/doc   "City/town"}
-                {:db/id    :address/state
-                 :db/ident :address/region
-                 :db/doc   "State/province/region."}
-                {:db/id                 #db/id[:db.part/db]
-                 :db/ident              :address/country
-                 :db/valueType          :db.type/string
-                 :db/cardinality        :db.cardinality/one
-                 :db/doc                "Country"
-                 :db.install/_attribute :db.part/db}]]
-    :requires [:starcity/add-address-schema
-               :starcity/seed-gilbert
-               :starcity/seed-mission
-               :starcity/seed-union-square
-               :starcity/seed-test-applications]}
+  (let [requires [:starcity/add-address-schema
+                  :starcity/seed-gilbert
+                  :starcity/seed-mission
+                  :starcity/seed-union-square]
+        requires (if (is-development?)
+                   (conj requires :starcity/seed-test-applications)
+                   requires)]
+    {:schema/alter-address-schema-10-8-16
+     {:txes     [[{:db/id    :address/city
+                   :db/ident :address/locality
+                   :db/doc   "City/town"}
+                  {:db/id    :address/state
+                   :db/ident :address/region
+                   :db/doc   "State/province/region."}
+                  {:db/id                 #db/id[:db.part/db]
+                   :db/ident              :address/country
+                   :db/valueType          :db.type/string
+                   :db/cardinality        :db.cardinality/one
+                   :db/doc                "Country"
+                   :db.install/_attribute :db.part/db}]]
+      :requires requires}
 
-   :seed/add-countries-to-addresses-10-8-16
-   {:txes [(fn [conn]
-             ;; Just find them all -- doesn't really matter which attr we use
-             (let [addresses (find-all-by (d/db conn) :address/locality)]
-               (mapv
-                (fn [{id :db/id}]
-                  [:db/add id :address/country "US"])
-                addresses)))]
-    :requires [:schema/alter-address-schema-10-8-16]}})
+     :seed/add-countries-to-addresses-10-8-16
+     {:txes     [(fn [conn]
+                   ;; Just find them all -- doesn't really matter which attr we use
+                   (let [addresses (find-all-by (d/db conn) :address/locality)]
+                     (mapv
+                      (fn [{id :db/id}]
+                        [:db/add id :address/country "US"])
+                      addresses)))]
+      :requires [:schema/alter-address-schema-10-8-16]}}))
