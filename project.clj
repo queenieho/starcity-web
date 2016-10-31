9c1117e6f654381e123b13830e61be93d0f26b1b
Josh Lehman <jalehman37@gmail.com>
2016-10-30 21:01:22 -0700

    begin next development cycle

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject starcity "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject starcity "1.0.2"
+  :description "The web app for https://joinstarcity.com"
+  :url "https://joinstarcity.com"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
