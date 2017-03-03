b7625447fb56adfa28605c0a7b53a8251993440d
Josh Lehman <jalehman37@gmail.com>
2017-03-03 13:25:37 -0800

    next dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity "1.4.1"
+(defproject starcity "1.4.2-SNAPSHOT"
   :description "The web app for https://joinstarcity.com"
   :url "https://joinstarcity.com"
   :license {:name "Eclipse Public License"
