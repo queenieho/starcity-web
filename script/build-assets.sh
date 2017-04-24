5f615f0feda21341a37f8be4e633c53e35ce286b
Josh Lehman <jalehman37@gmail.com>
2017-04-24 09:01:40 -0700

    add char encoding to sass compilation

--- script/build-assets.sh
+++ script/build-assets.sh
@@ -5,7 +5,7 @@ npm install
 echo "Compiling ant design LESS..."
 lessc --clean-css style/less/antd.less resources/public/assets/css/antd.css
 echo "Compiling public-facing SASS..."
-sass style/sass/public.scss:resources/public/assets/css/public.css
+sass -E "UTF-8" style/sass/public.scss:resources/public/assets/css/public.css
 echo "Compiling internal SASS..."
 # TODO: Rename starcity.css to something else
-sass style/sass/main.sass:resources/public/assets/css/starcity.css --style compressed
+sass -E "UTF-8" style/sass/main.sass:resources/public/assets/css/starcity.css --style compressed
