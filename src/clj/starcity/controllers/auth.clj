b3d61f577dff7c4cabea07233609855365a54cd8
Josh Lehman <jalehman37@gmail.com>
2017-07-10 16:26:06 -0700

    move password reset to reactor

--- src/clj/starcity/controllers/auth.clj
+++ src/clj/starcity/controllers/auth.clj
@@ -1,52 +1,28 @@
 (ns starcity.controllers.auth
   (:require [clojure.string :as str]
-            [customs.auth :as auth]
             [datomic.api :as d]
             [facade.core :as facade]
             [net.cgrand.enlive-html :as html]
-            [ring.util
-             [codec :refer [url-encode]]
-             [response :as response]]
-            [starcity
-             [config :as config :refer [config]]
-             [datomic :refer [conn]]]
+            [reactor.events :as events]
+            [ring.util.response :as response]
             [starcity.controllers.common :as common]
-            [starcity.models.account :as account]
-            [starcity.services.mailgun :as mail]
-            [starcity.services.mailgun.message :as mm]))
-
-;; =============================================================================
-;; Helpers
-;; =============================================================================
-
-(defn- send-password-reset-email
-  [account new-password]
-  (let [email (account/email account)]
-    (mail/send
-     email
-     "Starcity Password Reset"
-     (mm/msg
-      (mm/greeting (account/first-name account))
-      (mm/p "As requrested, we have reset your password. Your temporary password is:")
-      (mm/p (str "<b>" new-password "</b>"))
-      (mm/p
-       (format "After logging in <a href='%s'>here</a>, please change your
-       password to something more memorable by clicking on <b>My Account</b> in the upper right-hand corner of the page."
-               (format "%s/login?email=%s&next=/account" (config/hostname config) (url-encode email))))
-      (mm/p "If this was not you, please contact us at <a href='mailto:team@joinstarcity.com>team@joinstarcity.com</a>.")
-      (mm/signature)))))
+            [starcity.datomic :refer [conn]]
+            [starcity.models.account :as account]))
 
 ;; =============================================================================
 ;; Views
 ;; =============================================================================
 
+
 (html/defsnippet forgot-password-main "templates/forgot-password.html" [:main]
   [& errors]
   [:div.alerts] (facade/maybe-errors errors))
 
+
 ;; =============================================================================
 ;; Handlers
 ;; =============================================================================
+
 
 (defn logout
   "Log the user out by clearing the session."
@@ -58,6 +34,7 @@
       ;; same? Perhaps submit a PR?
       (assoc :session nil)))
 
+
 (defn- forgot-password-errors
   [req & errors]
   (common/render-malformed
@@ -66,6 +43,7 @@
                   :js-bundles ["main.js"]
                   :main (apply forgot-password-main errors))))
 
+
 (defn show-forgot-password [req]
   (common/render-ok
    (facade/public req
@@ -73,15 +51,14 @@
                   :js-bundles ["main.js"]
                   :main (forgot-password-main))))
 
+
 (defn forgot-password
   [{:keys [params] :as req}]
   (if-let [email (:email params)]
     (let [cleaned (-> email str/trim str/lower-case)]
       (if-let [account (account/by-email (d/db conn) cleaned)]
-        (let [[new-password tx-data] (auth/reset-password account)
-              next                   (format "/login?email=%s&reset-password=true" cleaned)]
-          @(d/transact conn [tx-data])
-          (send-password-reset-email account new-password)
+        (let [next (format "/login?email=%s&reset-password=true" cleaned)]
+          @(d/transact conn [(events/reset-password account)])
           (response/redirect next))
         (forgot-password-errors req (format "We do not have an account under %s. Please try again, or create an account."
                                             cleaned))))
