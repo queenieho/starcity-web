7eafd42e5a56059ae70cc449376044f3c428e89d
Josh Lehman <jalehman37@gmail.com>
2017-07-25 16:16:57 -0700

    bump deps

--- project.clj
+++ project.clj
@@ -36,8 +36,8 @@
                  [mount "0.1.11"]
                  ;; Datomic
                  [io.rkn/conformity "0.4.0"]
-                 [starcity/blueprints "1.10.0-SNAPSHOT" :exclusions [com.datomic/datomic-free]]
-                 [starcity/reactor "0.3.0-SNAPSHOT"]
+                 [starcity/blueprints "1.10.0" :exclusions [com.datomic/datomic-free]]
+                 [starcity/reactor "0.3.0"]
                  ;; CLJS
                  [cljsjs/flatpickr "2.0.0-rc.7-0"]
                  [starcity/ant-ui "0.1.2" :exclusions [re-frame]]
@@ -57,7 +57,7 @@
                  [com.taoensso/nippy "2.12.2"]
                  [prismatic/plumbing "0.5.3"]
                  [starcity/toolbelt "0.1.7" :exclusions [com.datomic/datomic-free]]
-                 [starcity/ribbon "0.5.0-SNAPSHOT"]
+                 [starcity/ribbon "0.5.0"]
                  [enlive "1.1.6"]
                  [cljsjs/moment "2.17.1-0"]]
 
