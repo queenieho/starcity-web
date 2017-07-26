dfa438d1c2608299e3bba1e2d58c44fdba29d4d9
Josh Lehman <jalehman37@gmail.com>
2017-07-25 18:13:14 -0700

    rename `user_id` to `account` to comply with Stripe API 2017-05-25

--- src/clj/starcity/webhooks/stripe.clj
+++ src/clj/starcity/webhooks/stripe.clj
@@ -10,15 +10,15 @@
 
 (defn hook
   [{params :params :as req}]
-  (let [{:keys [id type livemode user_id]} params]
+  (let [{:keys [id type livemode account]} params]
     (when-not (some? (d/entity (d/db conn) [:event/id id]))
       ;; If we're in production...
       (if (config/is-production? config)
         ;; Only accept events that are sent in `livemode` in production
         (when livemode
-          @(d/transact-async conn [(events/stripe-event id type user_id)]))
+          @(d/transact-async conn [(events/stripe-event id type account)]))
         ;; Accept all events during development
-        @(d/transact-async conn [(events/stripe-event id type user_id)])))
+        @(d/transact-async conn [(events/stripe-event id type account)])))
     ;; Acknowledge that we've received the event.
     (response/response {})))
 
@@ -29,7 +29,7 @@
   (do
     (def event* {:id      "evt_19hS9QJDow24Tc1atld0vATs"
                  :type    "customer.subscription.trial_will_end"
-                 :user_id "acct_191838JDow24Tc1a"})
+                 :account "acct_191838JDow24Tc1a"})
 
     @(http/post "http://localhost:8080/webhooks/stripe"
                 {:headers {"Content-Type" "application/json"}
