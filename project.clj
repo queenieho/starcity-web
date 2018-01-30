219519013d37ef8b865894a48b85e5c4b01c1b26
Josh Lehman <jalehman37@gmail.com>
2018-01-29 21:45:31 -0800

    Version 2.10.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity "2.10.1"
+(defproject starcity "2.10.2-SNAPSHOT"
   :description "The web app for https://starcity.com"
   :url "https://starcity.com"
   :license {:name "Eclipse Public License"
